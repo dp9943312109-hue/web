724739caede2c3af0f0433f16b2b2f3e0acd8568
dp9943312109-hue <dp9943312109@gmail.com>
2026-03-11 15:26:51 +0530

    Enhance Streamlit app with user input feature
    Added a text input for user name and a submit button.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("hello i am working in streamlit")
-st.weite("Hello! Creating a simple web")
+name = st.text_input("Enter your name:")
+if st.button("submit"):
+st.weite(f'Hello!{name} Creating a simple web")
+
